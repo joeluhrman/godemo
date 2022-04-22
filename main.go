9d3e26f9854f82e209faf14215c623e1500b49a3
Joe Luhrman <jaluhrman2018@gmail.com>
2022-04-21 20:18:56 -0800

    afdasdf

--- main.go
+++ main.go
@@ -14,25 +14,31 @@ import (
 	"math/rand"
 )
 
-const N = 10 // compiler will infer int
-const LEN = 5
+const (
+	N   = 10 // compiler will infer int
+	LEN = 5
+)
 
 func main() { // curly brace must be on this line
 	var arrays [N][LEN]int
 	var avgs [N]float32
 
+	// generating arrays
 	for i := 0; i < N; i++ {
 		data := make(chan [LEN]int)             // channel for int arrays
 		go func() { data <- generateArray() }() // goroutine to generate each array concurrently
 		arrays[i] = <-data
 	}
 
+	// calculating averages
 	for i := 0; i < N; i++ {
 		data := make(chan float32)                 // channel for floats
 		go func() { data <- average(arrays[i]) }() // goroutine to calculate each average concurrently
 		avgs[i] = <-data
 	}
 
+	// -----------------------------------------------------------------
+	// OUTPUT
 	fmt.Println("Arrays: ")
 	for i := 0; i < N; i++ {
 		fmt.Println("Array ", i, ": ", arrays[i])
